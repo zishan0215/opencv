a61d9ecc692f2080f90a14651ab4211990affc24
Zishan Ahmad <zishanahmad@outlook.com>
2016-02-06 17:30:06 +0530

    sharpening by kernel filter

--- sharpen.cpp
+++ sharpen.cpp
@@ -2,6 +2,7 @@
 #include <opencv2/opencv.hpp>
 
 void sharpen(const cv::Mat&, cv::Mat&);
+void sharpen2D(const cv::Mat&, cv::Mat&);
 
 int main(int argc, char **argv) {
 
@@ -20,14 +21,19 @@ int main(int argc, char **argv) {
 
     cv::Mat duplicate;
 
-    // cv::namedWindow("Original Image", cv::WINDOW_AUTOSIZE );
-    // cv::imshow("Original Image", image);
+    cv::namedWindow("Original Image", cv::WINDOW_AUTOSIZE );
+    cv::imshow("Original Image", image);
 
     sharpen(image, duplicate);
 
     cv::namedWindow("Sharpened Image", cv::WINDOW_AUTOSIZE );
     cv::imshow("Sharpened Image", duplicate);
 
+    image = cv::imread(argv[1], 0);
+    sharpen2D(image, duplicate);
+
+    cv::namedWindow("Sharpened Image kernel", cv::WINDOW_AUTOSIZE );
+    cv::imshow("Sharpened Image kernel", duplicate);
 
     cv::waitKey(0);
 	return 0;
@@ -58,5 +64,21 @@ void sharpen(const cv::Mat& image, cv::Mat& result) {
 	result.row(result.rows-1).setTo(cv::Scalar(0));
 	result.col(0).setTo(cv::Scalar(0));
 	result.col(result.cols-1).setTo(cv::Scalar(0)); 
+
+}
+
+void sharpen2D(const cv::Mat &image, cv::Mat &result) {
+
+	// Construct kernel (all entries initialized to 0)
+	cv::Mat kernel(3,3,CV_32F,cv::Scalar(0));
+	// assigns kernel values
+	kernel.at<float>(1,1)= 5.0;
+	kernel.at<float>(0,1)= -1.0;
+	kernel.at<float>(2,1)= -1.0;
+	kernel.at<float>(1,0)= -1.0;
+	kernel.at<float>(1,2)= -1.0;
+
+	//filter the image
+	cv::filter2D(image,result,image.depth(),kernel);
 
 }

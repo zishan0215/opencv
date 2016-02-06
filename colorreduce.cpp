3bfb0e10b2eb4022ff5ab91059497535f1089738
Zishan Ahmad <zishanahmad@outlook.com>
2016-02-06 11:50:33 +0530

    using iterators for color reduce

--- colorreduce.cpp
+++ colorreduce.cpp
@@ -1,6 +1,7 @@
 #include <opencv2/opencv.hpp>
 
 void color_reduce(cv::Mat& image, int div=64);
+void color_reduce_itertor(cv::Mat& image, int div=64);
 
 int main(int argc, char **argv) {
 
@@ -17,10 +18,18 @@ int main(int argc, char **argv) {
         return -1;
     }
 
+    cv::Mat duplicate = image.clone();
+
     color_reduce(image);
 
     cv::namedWindow("Reduced Color Image", cv::WINDOW_AUTOSIZE );
     cv::imshow("Reduced Color Image", image);
+
+    color_reduce(duplicate);
+
+    cv::namedWindow("Reduced Color Duplicate Image", cv::WINDOW_AUTOSIZE );
+    cv::imshow("Reduced Color Duplicate Image", duplicate);
+
 
     cv::waitKey(0);
 	return 0;
@@ -35,5 +44,16 @@ void color_reduce(cv::Mat& image, int div) {
 		for(int i=0; i<nc; i++) {
 			data[i] = (data[i]/div)*div + div/2;
 		}
+	}
+}
+
+void color_reduce_itertor(cv::Mat& image, int div) {
+	cv::Mat_<cv::Vec3b>::iterator it = image.begin<cv::Vec3b>();
+	cv::Mat_<cv::Vec3b>::iterator it_end = image.end<cv::Vec3b>();
+
+	for(; it != it_end; it++) {
+		(*it)[0] = ((*it)[0]/div)*div + div/2;
+		(*it)[1] = ((*it)[1]/div)*div + div/2;
+		(*it)[2] = ((*it)[2]/div)*div + div/2;
 	}
 }
